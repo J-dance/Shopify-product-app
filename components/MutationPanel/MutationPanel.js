2d28653147b0b599ddb3393309ba4d2315332df7
J-dance <dance.jojo@gmail.com>
2022-03-18 17:00:20 +0000

    added mutation panel toast on success

--- components/MutationPanel/MutationPanel.js
+++ components/MutationPanel/MutationPanel.js
@@ -1,18 +1,31 @@
 import { useMutation } from 'react-apollo';
 import { useEffect } from 'react';
 import { Spinner, Stack } from '@shopify/polaris';
+import { useAppBridge } from "@shopify/app-bridge-react";
+import {Toast} from '@shopify/app-bridge/actions';
 
 const MutationPanel = (props) => {
   const { MUTATION, input, onCompletedAction } = props;
+  const app = useAppBridge();
 
   const [runMutation, { data, loading, error }] = useMutation(MUTATION, {
     "variables": input,
-    onCompleted:() => { onCompletedAction() }
+    onCompleted:() => { 
+      toastNotice.dispatch(Toast.Action.SHOW);
+      onCompletedAction(); 
+    }
   });
 
   useEffect(() => {
     runMutation();
   }, [runMutation]);
+
+  const toastOptions = {
+    message: 'Successful',
+    duration: 5000,
+  };
+
+  const toastNotice = Toast.create(app, toastOptions);
 
   error && console.log(error)
   data && console.log(data);
