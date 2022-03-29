dd8d45b21b6e7670a15f9262a8c1991bf6f4f0b1
J-dance <dance.jojo@gmail.com>
2022-03-29 11:04:04 +0100

    changed next config back to orinial require

--- next.config.js
+++ next.config.js
@@ -1,27 +1,30 @@
 const { parsed: localEnv } = require("dotenv").config();
 
-import { DefinePlugin } from "webpack";
+const webpack = require("webpack");
 const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
 const securityHeaders = [ 
   { 
     key: "Content-Security-Policy",
     value: `frame-ancestors https://${process.env.SHOP} https://admin.shopify.com`
   },
-]
+];
 
-export function webpack(config) {
-  const env = { API_KEY: apiKey };
-  config.plugins.push(new DefinePlugin(env));
+module.exports = {
+  webpack: (config) => {
+    const env = { API_KEY: apiKey };
+    config.plugins.push(new webpack.DefinePlugin(env));
 
-  // Add ESM support for .mjs files in webpack 4
-  config.module.rules.push({
-    test: /\.mjs$/,
-    include: /node_modules/,
-    type: "javascript/auto",
-  });
+    // Add ESM support for .mjs files in webpack 4
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
 
-  return config;
-}
+    return config;
+  },
+};
+
 export async function headers() {
   return [
     {
